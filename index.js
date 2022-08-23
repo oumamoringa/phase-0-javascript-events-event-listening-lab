0dde58c55f8ef8158833cb4f52f69955699cfc60
oumamoringa <richard.ouma@student.moringaschool>
2022-08-23 08:39:43 +0300

    Completed assignmemt

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function addingEventListener() {
   const input = document.getElementById('button')
 
-  function clickAlert() {
-    alert('I was clicked!')
-  }
+  const clickAlert = () => alert('I was clicked!')
 
   input.addEventListener('click', clickAlert)
 }
